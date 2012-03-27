116933409f6c4d71bcae38d3122703a5f3783314
T3hGamerDK <tcg.thegamer@gmail.com>
2012-03-27 16:22:18 +0200

    (Hopefully) fixed project generation errors. Should compile fine now.

--- build/premake4.lua
+++ build/premake4.lua
@@ -7,8 +7,6 @@ solution "PASSION"
 	configuration "Release"
 		flags { "Optimize" }
 
-	includedirs { "../include", "../extlibs/headers" }
-	libdirs { "../extlibs/lib", "../lib", "../passion/bin/bin" }
 	targetdir( "../passion/bin/bin" )
 	if( _ACTION == nil ) then
 		location "projects/"
@@ -20,25 +18,31 @@ solution "PASSION"
 	project "input"
 		kind "SharedLib"
 		language "C++"
+		includedirs { "../include" }
 		files { "../src/Passion/Input/**.cpp" }
 	project "render"
 		kind "SharedLib"
 		language "C++"
+		includedirs { "../include", "../extlibs/headers" }
+		libdirs { "../extlibs/lib" }
 		files { "../src/Passion/Render/**.cpp" }
 		links { "GL", "SOIL" }
 	project "network"
 		kind "SharedLib"
 		language "C++"
+		includedirs { "../include" }
 		files { "../src/Passion/Network/**.cpp" }
 	project "scripting"
 		kind "SharedLib"
 		language "C++"
+		includedirs { "../include", "../extlibs/headers" }
 		files { "../src/Passion/Scripting/**.cpp" }
 		links { "dl", "lua" }
 	project "passion"
 		kind "WindowedApp"
 		language "C++"
 		targetdir( "../passion/bin" )
+		libdirs { "../extlibs/lib", "../passion/bin/bin" }
+		includedirs { "../include", "../extlibs/headers" }
 		files { "../passion/src/**.cpp", "../src/Passion/Render/GLee/GLee.c" }
-		--links { "input", "GL", "GLU", "SOIL", "render", "network", "dl", "scripting" }
 		links { "GL", "GLU", "SOIL", "dl" }
